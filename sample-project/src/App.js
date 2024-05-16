db2a029e7cd53e0ff2fb9e539c58746a410b5c59
Nizara-DNS <67256107+Nishara-Sewminie@users.noreply.github.com>
2024-05-16 11:20:25 +0530

    cards added

--- sample-project/src/App.js
+++ sample-project/src/App.js
@@ -41,58 +41,94 @@ function App() {
           </Navbar.Collapse>
         </Container>
       </Navbar>
-                                                  {/* Jumborton starts here */}
+      {/* Jumborton starts here */}
       <div class="bg-primary text-white">
         <p>15% offer! for newly Registered Accounts.. &nbsp;<u>Claim</u> your membership today!</p>
       </div>
 
       {/* <!-- Carousel --> */}
-<div id="demo" class="carousel slide" data-bs-ride="carousel">
+      <div id="demo" class="carousel slide" data-bs-ride="carousel">
 
-  {/* <!-- Indicators/dots --> */}
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#demo" data-bs-slide-to="0" class="active"></button>
-    <button type="button" data-bs-target="#demo" data-bs-slide-to="1"></button>
-    <button type="button" data-bs-target="#demo" data-bs-slide-to="2"></button>
-  </div>
-  
-  {/* <!-- The slideshow/carousel --> */}
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src={one} alt="Los Angeles" class="d-block" style={{width:"100%", height:"500px"}}/>
-      <div class="carousel-caption">
-        <h3>Los Angeles</h3>
-        <p>We had such a great time in LA!</p>
+        {/* <!-- Indicators/dots --> */}
+        <div class="carousel-indicators">
+          <button type="button" data-bs-target="#demo" data-bs-slide-to="0" class="active"></button>
+          <button type="button" data-bs-target="#demo" data-bs-slide-to="1"></button>
+          <button type="button" data-bs-target="#demo" data-bs-slide-to="2"></button>
+        </div>
+
+        {/* <!-- The slideshow/carousel --> */}
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src={one} alt="Los Angeles" class="d-block" style={{ width: "100%", height: "500px" }} />
+            <div class="carousel-caption">
+              <h3>Sample 1</h3>
+              <p>Description of Sample 1</p>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img src={two} alt="Chicago" class="d-block" style={{ width: "100%", height: "500px" }} />
+            <div class="carousel-caption">
+              <h3>Sample 2</h3>
+              <p>Description of Sample 2</p>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img src={three} alt="New York" class="d-block" style={{ width: "100%", height: "500px" }} />
+            <div class="carousel-caption">
+              <h3>Sample 3</h3>
+              <p>Description of Sample 3</p>
+            </div>
+          </div>
+        </div>
+
+        {/* <!-- Left and right controls/icons --> */}
+        <button class="carousel-control-prev" type="button" data-bs-target="#demo" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon"></span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#demo" data-bs-slide="next">
+          <span class="carousel-control-next-icon"></span>
+        </button>
       </div>
-    </div>
-    <div class="carousel-item">
-  <img src={two} alt="Chicago" class="d-block" style={{width:"100%", height:"500px"}}/>
-      <div class="carousel-caption">
-        <h3>Chicago</h3>
-        <p>Thank you, Chicago!</p>
-      </div> 
-    </div>
-    <div class="carousel-item">
-      <img src={three} alt="New York" class="d-block" style={{width:"100%", height:"500px"}}/>
-      <div class="carousel-caption">
-        <h3>New York</h3>
-        <p>We love the Big Apple!</p>
-      </div>  
-    </div>
-  </div>
-  
-  {/* <!-- Left and right controls/icons --> */}
-  <button class="carousel-control-prev" type="button" data-bs-target="#demo" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon"></span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#demo" data-bs-slide="next">
-    <span class="carousel-control-next-icon"></span>
-  </button>
-</div>
 
 
-                        {/* Services starts here */}
-                        
+      {/* Services starts here */}
+      <div class="d-inline-flex p-3">
+
+      <div class="container mt-3">
+        <div class="card" style={{ width: "400px" }}>
+          <img class="card-img-top" src={three} alt="Card image" style={{ width: "100%" }} />
+          <div class="card-body">
+            <h4 class="card-title">Product 1</h4>
+            <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+            <a href="#" class="btn btn-primary">See More</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="container mt-3">
+        <div class="card" style={{ width: "400px" }}>
+          <img class="card-img-top" src={three} alt="Card image" style={{ width: "100%" }} />
+          <div class="card-body">
+            <h4 class="card-title">Product 2</h4>
+            <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+            <a href="#" class="btn btn-primary">See More</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="container mt-3">
+        <div class="card" style={{ width: "400px" }}>
+          <img class="card-img-top" src={three} alt="Card image" style={{ width: "100%" }} />
+          <div class="card-body">
+            <h4 class="card-title">Product 3</h4>
+            <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+            <a href="#" class="btn btn-primary">See More</a>
+          </div>
+        </div>
+      </div>
+
+      
+      </div>
 
     </div>
   );
