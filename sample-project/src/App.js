87cb4c23e5330f70cd863ac2bf5dfd7eb9e512c4
Nizara-DNS <67256107+Nishara-Sewminie@users.noreply.github.com>
2024-05-16 10:55:17 +0530

    Jumbortan and carousel added

--- sample-project/src/App.js
+++ sample-project/src/App.js
@@ -4,6 +4,9 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import one from './images/one.jpg';
+import two from './images/two.jpg';
+import three from './images/three.jpg';
 
 
 function App() {
@@ -11,13 +14,13 @@ function App() {
     <div className="App">
       <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
         <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Brand href="#home">Brand Name</Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
               <Nav.Link href="#features">Features</Nav.Link>
               <Nav.Link href="#pricing">Pricing</Nav.Link>
-              <NavDropdown title="Dropdown" id="collapsible-nav-dropdown">
+              <NavDropdown title="Services" id="collapsible-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">
                   Another action
@@ -30,14 +33,66 @@ function App() {
               </NavDropdown>
             </Nav>
             <Nav>
-              <Nav.Link href="#deets">More deets</Nav.Link>
+              <Nav.Link href="#deets">Contact</Nav.Link>
               <Nav.Link eventKey={2} href="#memes">
-                Dank memes
+                Visit Store
               </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
+                                                  {/* Jumborton starts here */}
+      <div class="bg-primary text-white">
+        <p>15% offer! for newly Registered Accounts.. &nbsp;<u>Claim</u> your membership today!</p>
+      </div>
+
+      {/* <!-- Carousel --> */}
+<div id="demo" class="carousel slide" data-bs-ride="carousel">
+
+  {/* <!-- Indicators/dots --> */}
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#demo" data-bs-slide-to="0" class="active"></button>
+    <button type="button" data-bs-target="#demo" data-bs-slide-to="1"></button>
+    <button type="button" data-bs-target="#demo" data-bs-slide-to="2"></button>
+  </div>
+  
+  {/* <!-- The slideshow/carousel --> */}
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src={one} alt="Los Angeles" class="d-block" style={{width:"100%", height:"500px"}}/>
+      <div class="carousel-caption">
+        <h3>Los Angeles</h3>
+        <p>We had such a great time in LA!</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+  <img src={two} alt="Chicago" class="d-block" style={{width:"100%", height:"500px"}}/>
+      <div class="carousel-caption">
+        <h3>Chicago</h3>
+        <p>Thank you, Chicago!</p>
+      </div> 
+    </div>
+    <div class="carousel-item">
+      <img src={three} alt="New York" class="d-block" style={{width:"100%", height:"500px"}}/>
+      <div class="carousel-caption">
+        <h3>New York</h3>
+        <p>We love the Big Apple!</p>
+      </div>  
+    </div>
+  </div>
+  
+  {/* <!-- Left and right controls/icons --> */}
+  <button class="carousel-control-prev" type="button" data-bs-target="#demo" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#demo" data-bs-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </button>
+</div>
+
+
+                        {/* Services starts here */}
+                        
 
     </div>
   );
